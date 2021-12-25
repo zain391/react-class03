367a4c8b672b26c1fa21dc6c749a455e245c865d
zain391 <zainqamar391@gmail.com>
2021-12-25 05:48:47 -0800

    we are good to go with class one code
    we are good to go with class one code

---
+++ class-03/src/App.js
@@ -0,0 +1,10 @@
+import FirstClass from "./class01/components-1";
+function App() {
+  return (
+    <div className="App">
+      <FirstClass/>
+    </div>
+  );
+}
+
+export default App;
---
+++ class-03/src/class01/components-1/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+import zain from "../img1/first.jpg"
+function FirstClass(){
+    return(
+        <div>
+            <img className="first" src={zain} alt="firstpic" />
+        </div>
+    )
+}
+export default FirstClass;
+// class-03\src\class01\img1\first.jpg
