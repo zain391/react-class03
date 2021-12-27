f8be7cf850bd71a724d1a30dd8bbcb3d24f0cea7
zain391 <zainqamar391@gmail.com>
2021-12-27 00:58:36 -0800

    class 3 index.js code for getting the value from input and displaying it in heading 1 using the use state from react
    class 3 index.js code for getting the value from input and displaying it in heading 1 using the use state from react

--- class-03/src/class-03/components-3/first-props/index.js
+++ class-03/src/class-03/components-3/first-props/index.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 function Welcome(){
     const [todoInput, setTodoInput]=useState("");
-    
+    function showing(){
+        setTodoInput(document.getElementById("todoInput").value);
+        console.log(todoInput);
+    }
     return(
         <div>
             <input type="text" placeholder="enter todo" id="todoInput"/>
-            <button  id="addbtn">ADD</button>
-            <h1></h1>
+            <button  id="addbtn" onClick={showing}>ADD</button>
+            <h1>{todoInput}</h1>
         </div>
     )
 }
